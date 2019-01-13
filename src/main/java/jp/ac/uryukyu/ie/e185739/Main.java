23423fec888ee7fa7efa7c4741b828d7bf2942a1
Choi Jeongho <e185739@ie.u-ryukyu.ac.jp>
2019-01-13 22:35:53 +0900

    ユーザの定価

--- src/main/java/jp/ac/uryukyu/ie/e185739/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185739/Main.java
@@ -12,8 +12,7 @@ class BaseBall{
     static int strike =0;
     static int ball = 0;
 
-    public static void main (String[] args) throws java.lang.Exception
-    {
+    public static void main (String[] args) throws java.lang.Exception {
         int computer[] = new int[3]; //パソコンの定価
         int user[] = new int[3]; //ユーザの定価
 
@@ -24,23 +23,43 @@ class BaseBall{
         //random.nextInt(10); これは 1~9間にランダム
 
         //computer 3番実行中に1番目が0ならランダム値を入れる
-        while(computer[0] == 0){
+        while (computer[0] == 0) {
             computer[0] = random.nextInt(10);
         }
 
         //computer 3中に1番目と2番目が重複したり,
         //2番目が0ならランダム値入れる
-        while(computer[0] == computer[1] || computer[1] == 0){
+        while (computer[0] == computer[1] || computer[1] == 0) {
             computer[1] = random.nextInt(10);
         }
 
         //computer 3中に1番目と3番目が重複したり,
         //2番目と3番目が重複すれば,
         //3番目が0ならランダム値入れる
-        while(computer[0] == computer[2] || computer[1] == computer[2] || computer[2] == 0){
+        while (computer[0] == computer[2] || computer[1] == computer[2] || computer[2] == 0) {
             computer[2] = random.nextInt(10);
         }
 
+        //ユーザの定価
 
+        Scanner s = new Scanner(System.in);
+
+        while (strike < 3) { //ストライク3になる時まで無限ループ
+
+            //3回繰り返してユーザーの入力を受ける。
+            for (int i = 0; i < user.length; i++) {
+                System.out.print("\n" + (i + 1) + "番目の数: ");
+                user[i] = s.nextInt();
+
+                //もし10以上だったり,0以下ならエラー出力
+                while (user[i] >= 10 || user[i] <= 0) {
+                    System.out.println("error: 1~9間に入力してください。");
+                    System.out.print(i + 1 + "番目の数: ");
+                    user[i] = s.nextInt();
+                }
+            }
+
+
+        }
     }
 }
