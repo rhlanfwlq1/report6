89a227de828aa7be85b56ea9b80244470810775f
Choi Jeongho <e185739@ie.u-ryukyu.ac.jp>
2019-01-13 21:59:29 +0900

    数字野球2

--- src/main/java/jp/ac/uryukyu/ie/e185739/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185739/Main.java
@@ -9,7 +9,16 @@ import java.util.Scanner;
 
 class BaseBall{
 
+    static int strike =0;
+    static int ball = 0;
+
     public static void main (String[] args) throws java.lang.Exception
     {
+        int computer[] = new int[3]; //パソコンの定価
+        int user[] = new int[3]; //ユーザの定価
+
+        Random random = new Random();
+        
+
     }
 }
