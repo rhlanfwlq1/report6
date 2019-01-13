1ec157d613633545b8b1bcb03c18e5f6aa521bca
Choi Jeongho <e185739@ie.u-ryukyu.ac.jp>
2019-01-13 21:22:10 +0900

    数字野球1

---
+++ src/main/java/jp/ac/uryukyu/ie/e185739/Main.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e185739;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class BaseBall{
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+    }
+}
